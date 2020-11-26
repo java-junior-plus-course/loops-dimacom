e94452536317b6d0a05f95ec32b2f5f69b8e6847
dima <karpov83d@gmail.com>
2020-11-27 02:29:10 +0300

    all loop finish.

--- src/main/java/edu/junior/DivisibilityExclusion.java
+++ src/main/java/edu/junior/DivisibilityExclusion.java
@@ -24,27 +24,98 @@ public class DivisibilityExclusion {
     }
 
     public int[] removeOdd() {
-        // TODO: Remove all odd numbers from the array with for loop
-        throw new UnsupportedOperationException();
+        int j = 0;
+        int evenCount = 0;
+
+        int numbersLength = numbers.length;
+        for (int i = 0; i < numbersLength; i++) {
+            if (numbers[i] % 2 == 0) {
+                evenCount++;
+            }
+        }
+
+        int[] evenNumbers = new int[evenCount];
+
+        for (int i = 0; i < numbersLength; i++) {
+            if (numbers[i] % 2 == 0) {
+                evenNumbers[j++] = numbers[i];
+            }
+        }
+        return evenNumbers;
     }
 
     public int[] removeEven() {
-        // TODO: Remove all odd numbers from the array with for-each loop
-        throw new UnsupportedOperationException();
+        int countEven = 0;
+
+        for (int number : numbers) {
+            if (number % 2 != 0) {
+                countEven++;
+            }
+        }
+        int[] result = new int[countEven];
+        int i = 0;
+
+        for (int number : numbers) {
+            if (number % 2 != 0) {
+                result[i] = number;
+                i++;
+            }
+        }
+        return result;
     }
 
     public int[] removeEvenOrOdd(boolean odd) {
+        if (odd) {
+            int[] result = removeOdd();
+            return result;
+        } else {
+            int[] result = removeEven();
+            return result;
+        }
+
         // TODO: Remove all odd or even numbers (based on the second argument) from the array with for-each loop
-        throw new UnsupportedOperationException();
+        // throw new UnsupportedOperationException();
     }
 
     public int[] removeDivisibleByThree() {
-        // TODO: Remove all numbers which are divisible by three from the array with while or do-while loop
-        throw new UnsupportedOperationException();
+        int i = 0;
+        int numbersLength = numbers.length;
+        int countDivisibleThree = 0;
+        while (i < numbersLength) {
+            if (numbers[i] % 3 == 0) {
+                countDivisibleThree++;
+            }
+            i++;
+        }
+        int j = 0;
+        i = 0;
+        int[] result = new int[numbersLength - countDivisibleThree];
+        while (j < numbersLength) {
+            if (numbers[j] % 3 != 0) {
+                result[i++] = numbers[j];
+            }
+            j++;
+        }
+        return result;
     }
 
     public int[] removeDivisible(int divisor) {
-        // TODO: Remove all numbers which are divisible by the second argument
-        throw new UnsupportedOperationException();
+        int count = 0;
+        for (int number : numbers) {
+            if (number % divisor == 0) {
+                count++;
+            }
+        }
+        int resultLength = numbers.length - count;
+        int[] result = new int[resultLength];
+
+        int i = 0;
+        for (int number : numbers)
+            if (number % divisor != 0) {
+                result[i++] = number;
+            }
+
+        return result;
     }
+
 }
